01c7f72bc4927f3e9bfba5a994c8b1278823c297
Evgenii Nikitin <en919@nyu.edu>
2019-03-30 19:39:42 -0400

    remove joining

--- redditscaling/features/build_features_star.py
+++ redditscaling/features/build_features_star.py
@@ -96,8 +96,5 @@ if __name__ == "__main__":
     df_tokenized = tokenize_data(
         df_comments, args_dict['ignore_stopwords'], args_dict['keepcaps'],
         args_dict['decontract'], args_dict['remove_punct'])
-    df_tokenized = df_tokenized.groupby(
-        'subreddit')['body'].apply(lambda x: '\t'.join(x)).reset_index()
-    df_tokenized['body'] = '/r/' + \
-        df_tokenized['subreddit'] + '\t' + df_tokenized['body']
+    df_tokenized['subreddit'] = '__label__' + df_tokenized['subreddit']
     df_tokenized['body'].to_csv(args_dict['output_file'], index=False)
